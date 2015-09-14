596881b52b89bdf897e21766345ec6dadbf31739
mlobb-Home-Pc <matej.lobb@gmail.com>
2015-09-15 01:01:37 +0200

    Issue4 - .sql Correcting
    Signed-off-by: mlobb-Home-Pc <matej.lobb@gmail.com>

--- sk-app-db/CreateDatabase.sql
+++ sk-app-db/CreateDatabase.sql
@@ -1,41 +1,29 @@
-
--- Exportování struktury databáze pro
-CREATE DATABASE IF NOT EXISTS `db` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE IF NOT EXISTS `db`;
 USE `db`;
 
--- Exportování struktury pro tabulka db.roles
 CREATE TABLE IF NOT EXISTS `roles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `role_name` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+)
 
--- Exportování dat pro tabulku db.roles: ~3 rows (přibližně)
-/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
 INSERT INTO `roles` (`id`, `role_name`) VALUES
 	(1, 'ADMIN'),
 	(2, 'MODERATOR'),
 	(3, 'VIEW');
-/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
 
--- Exportování struktury pro tabulka db.user_details
 CREATE TABLE IF NOT EXISTS `user_details` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_name` text,
   `user_password` text,
   `user_full_name` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+);
 
--- Exportování dat pro tabulku db.user_details: ~2 rows (přibližně)
-/*!40000 ALTER TABLE `user_details` DISABLE KEYS */;
 INSERT INTO `user_details` (`id`, `user_name`, `user_password`, `user_full_name`) VALUES
-	(1, 'lobor', 'lobor', 'Lobor Test'),
+	(1, 'test', 'test', 'Test Test'),
 	(2, 'viewer', 'viewer', 'Viewer Test');
-/*!40000 ALTER TABLE `user_details` ENABLE KEYS */;
 
-
--- Exportování struktury pro tabulka db.user_roles
 CREATE TABLE IF NOT EXISTS `user_roles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) DEFAULT NULL,
@@ -45,15 +33,9 @@ CREATE TABLE IF NOT EXISTS `user_roles` (
   KEY `FK_user_idx` (`user_id`),
   CONSTRAINT `FK_role` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `FK_user` FOREIGN KEY (`user_id`) REFERENCES `user_details` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+);
 
--- Exportování dat pro tabulku db.user_roles: ~2 rows (přibližně)
-/*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
 INSERT INTO `user_roles` (`id`, `user_id`, `role_id`) VALUES
 	(1, 1, 1),
 	(2, 1, 2),
 	(3, 2, 3);
-/*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
